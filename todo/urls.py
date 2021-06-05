fe620950e7cd3492436dd0de7e9d7ee7a6b22741
sahad mukkam <sahadmukkam19@gmail.com>
2021-06-05 20:43:22 +0530

    start frontend

--- todo/urls.py
+++ todo/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/',include('api.urls'))
+    path('todo/',include('api.urls')),
+    path('',include('frontend.urls'))
 ]
